482038a695ed230b7156ae3b6f2473eb2fba2548
Maxwell Wei <y238wei@uwaterloo.ca>
2023-11-15 10:17:54 -0500

    improve human supervise

--- AssessmentForcast.py
+++ AssessmentForcast.py
@@ -1,7 +1,7 @@
 from EVLPCases import *
 import numpy as np
 import pandas as pd
-
+import pickle
 
 VENTILATOR_RAW_TS_FOLDER = r"C:\Users\weiyi\Workspace\UHN\ventilator data"
 DONOR_INFO_FILE_PATH = r"C:\Users\weiyi\Workspace\UHN\EVLP Data\EVLP#1-879_donor.csv"
@@ -14,24 +14,28 @@ cases = [551, 553, 554, 555, 557, 558, 560, 563, 564, 565, 568, 572, 573,
          617, 618, 619, 621, 631, 682, 685, 686, 694, 698, 730, 731, 736,
          738, 753, 762, 782, 803, 817, 818]
 
-
 evlp_cases = EVLPCases(VENTILATOR_RAW_TS_FOLDER,
                        DONOR_INFO_FILE_PATH, RECIPIENT_INFO_FILE_PATH,
                        TRANSPLANT_OUTCOME_FILE_PATH,
                        PROCESSED_CASE_SAVE_FOLDER, cases)
 
 
-def get_assessment_periods(case, tol=0.5, consecutive_breaths=20):
+def get_assessment_periods(case, duration_tol=0.75, consecutive_breaths=20, noise_tol=3):
     """Get the assessment periods of the case."""
     assessment_periods = []
     # a valid assessment period should have at least 20 breaths with duration between 5.5 and 6.5 seconds
     breath_idx = 1
     while breath_idx <= case.ventilator_per_breath_ts.n_breaths - consecutive_breaths:
-        durations = case.per_breath_param_table.param_table.loc[breath_idx:breath_idx+consecutive_breaths-1, "Duration(s)"]
-        if abs(durations.loc[breath_idx] - 6) < tol and np.all(np.abs(durations - 6) < tol):
+        durations = case.per_breath_param_table.param_table.loc[breath_idx:breath_idx + consecutive_breaths - 1,
+                    "Duration(s)"]
+        if abs(durations.loc[breath_idx] - 6) < duration_tol and np.sum(
+                np.abs(durations - 6) < duration_tol) > consecutive_breaths - noise_tol:
             # assessment period ends at the last consecutive breath with duration between 5.5 and 6.5 seconds
             end_idx = breath_idx + consecutive_breaths - 1
-            while np.any(np.abs(case.per_breath_param_table.param_table.loc[end_idx+1:end_idx+consecutive_breaths, "Duration(s)"] - 6) < tol)\
+            while (np.abs(case.per_breath_param_table.param_table.loc[end_idx + 1, "Duration(s)"] - 6) < duration_tol \
+                   or np.sum(np.abs(
+                        case.per_breath_param_table.param_table.loc[end_idx + 1:end_idx + consecutive_breaths,
+                        "Duration(s)"] - 6) < duration_tol) > noise_tol) \
                     and end_idx < case.ventilator_per_breath_ts.n_breaths - 1:
                 end_idx += 1
             assessment_periods.append((breath_idx, end_idx))
@@ -41,12 +45,25 @@ def get_assessment_periods(case, tol=0.5, consecutive_breaths=20):
     return assessment_periods
 
 
-assessment_periods_per_case = {}
+# assessment_periods_per_case = {"Good": {}, "OK": {}, "Bad": {}}
+with open("assessment_periods_per_case.pkl", "rb") as f:
+    assessment_periods_per_case = pickle.load(f)
+
 for case in evlp_cases.cases.values():
-    tol = 0.5
+    if case.case_id in assessment_periods_per_case["Good"]:
+        print(f"Case {case.case_id} already marked as Good.")
+        continue
+    elif case.case_id in assessment_periods_per_case["OK"]:
+        print(f"Case {case.case_id} already marked as OK.")
+        continue
+    elif case.case_id in assessment_periods_per_case["Bad"]:
+        print(f"Case {case.case_id} already marked as Bad.")
+        continue
+    duration_tol = 0.75
     consecutive_breaths = 20
+    niose_tol = 3
     while True:
-        assessment_periods = get_assessment_periods(case, tol, consecutive_breaths)
+        assessment_periods = get_assessment_periods(case, duration_tol, consecutive_breaths)
         print(case.case_id)
         print(assessment_periods)
         x = case.per_breath_param_table.param_table.index.to_numpy()
@@ -56,18 +73,24 @@ for case in evlp_cases.cases.values():
             plt.axvspan(start_idx, end_idx, color="grey", alpha=0.5)
         plt.tight_layout()
         plt.show()
-        option = input("Accept? (y/n/skip) ")
-        if option == "y":
-            break
-        elif option == "skip":
+        option = input("Options:\n\tG: Good\n\tO: OK\n\tB: Bad\n\tAP: Adjust Parameters\nInput: ")
+        if option in ["G", "O", "B"]:
+            if option == "G":
+                assessment_periods_per_case["Good"][case.case_id] = assessment_periods
+                print(f"Case {case.case_id} marked as Good.")
+            elif option == "O":
+                assessment_periods_per_case["OK"][case.case_id] = assessment_periods
+                print(f"Case {case.case_id} marked as OK.")
+            else:
+                assessment_periods_per_case["Bad"][case.case_id] = assessment_periods
+                print(f"Case {case.case_id} marked as Bad.")
             break
         else:
-            tol = float(input("tol: "))
+            duration_tol = float(input("duration_tol: "))
             consecutive_breaths = int(input("consecutive_breaths: "))
+            niose_tol = int(input("niose_tol: "))
 
     assessment_periods_per_case[case.case_id] = assessment_periods
 
-
-print(assessment_periods_per_case)
 with open("assessment_periods_per_case.pkl", "wb") as f:
     pickle.dump(assessment_periods_per_case, f)
